a9d163a9eb955d6fe989abda83dd6ef635870c15
Adarsh Pandey <itsadarshvijaypandey@gmail.com>
2025-04-28 21:02:33 +0530

    added glow effect

--- lib/widgets/profile_button.dart
+++ lib/widgets/profile_button.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:portfolio/utils/colors/custom_colors.dart';
 import 'package:portfolio/utils/helper_functions.dart';
 
-class ProfileButton extends StatelessWidget {
+class ProfileButton extends StatefulWidget {
   final String title;
   final String url;
   final IconData icon;
@@ -15,25 +15,67 @@ class ProfileButton extends StatelessWidget {
   });
 
   @override
+  State<ProfileButton> createState() => _ProfileButtonState();
+}
+
+class _ProfileButtonState extends State<ProfileButton> {
+  var isHovering = false;
+  @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: CupertinoButton(
-        onPressed: () {
-          HelperFunctions.launchCustomURL(url: url);
-        },
-        color: CustomColors.lightCardColor,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(icon, color: CustomColors.accentColor),
-            const SizedBox(width: 10),
-            Text(
-              title,
-              style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                color: CustomColors.labelTextColor,
-              ),
+      child: MouseRegion(
+        onEnter:
+            (event) => setState(() {
+              isHovering = true;
+            }),
+        onExit:
+            (event) => setState(() {
+              isHovering = false;
+            }),
+        child: Container(
+          decoration: BoxDecoration(
+            boxShadow: [
+              if (isHovering)
+                BoxShadow(
+                  color: CustomColors.accentColor.withOpacity(0.6),
+                  blurRadius: 20,
+                  spreadRadius: 2,
+                  offset: const Offset(0, 0),
+                ),
+            ],
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: CupertinoButton(
+            onPressed: () {
+              HelperFunctions.launchCustomURL(url: widget.url);
+            },
+            color:
+                isHovering
+                    ? CustomColors.accentColor
+                    : CustomColors.lightCardColor,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  widget.icon,
+                  color:
+                      isHovering
+                          ? CustomColors.cardColor
+                          : CustomColors.accentColor,
+                ),
+                const SizedBox(width: 10),
+                Text(
+                  widget.title,
+                  style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                    color:
+                        isHovering
+                            ? CustomColors.cardColor
+                            : CustomColors.labelTextColor,
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
